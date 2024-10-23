a65d4a50f3d5dedd7e02b1716e55749e3a388b38
Bladimir Cabrera <vladwithcode@gmail.com>
2024-10-23 17:53:48 -0600

    change: include internal/templates in tailwind

--- web/style/tailwind.config.js
+++ web/style/tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   relative: true,
-  content: ["web/templates/**/*.html"],
+  content: [
+      "web/templates/**/*.html",
+      "internal/templates/**/*.templ",
+  ],
   theme: {
     extend: {
         colors: {}
