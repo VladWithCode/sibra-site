6effc45d3c8e589f53e2a692a645b2e79948c563
vladwb <vladwithcode@gmail.com>
2025-09-13 15:43:03 -0600

    fix: inverse ignore rule won't work in .gitignore
    used git add -f for the moment
    TODO: fix the inverse ignore rule in .gitignore

---
+++ cmd/init/init-site.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	"github.com/vladwithcode/sibra-site/internal/db"
+)
+
+func main() {
+	err := godotenv.Overload()
+	if err != nil {
+		log.Fatalf("failed to load .env file: %v", err)
+	}
+	var (
+		user = os.Getenv("ADMIN_USERNAME")
+		pass = os.Getenv("ADMIN_PASSWORD")
+	)
+	if user == "" || pass == "" {
+		log.Fatalf("missing ADMIN_USERNAME or ADMIN_PASSWORD")
+	}
+
+	conn, err := db.Connect()
+	if err != nil {
+		log.Fatalf("failed to connect to db: %v", err)
+	}
+	defer conn.Close()
+
+	id, err := uuid.NewV7()
+	if err != nil {
+		log.Fatalf("failed to generate uuid: %v", err)
+	}
+	defaultUser := db.User{
+		Id:        id.String(),
+		Name:      "Administrador",
+		Username:  user,
+		Password:  pass,
+		Role:      db.RoleAdmin,
+		Email:     "admin@sibra.mx",
+		GoldenBoy: true,
+	}
+
+	_, err = db.CreateUser(&defaultUser)
+	if err != nil {
+		log.Fatalf("failed to create user: %v", err)
+	}
+
+	log.Printf("User created with id: %v", id.String())
+}
