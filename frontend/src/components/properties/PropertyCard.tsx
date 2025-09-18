926ebf6c461f1b048d54dc4a7f631fcf6d545bb4
vladwb <vladwithcode@gmail.com>
2025-09-18 00:54:36 -0600

    feat: PropertyCard component

--- frontend/src/components/properties/PropertyCard.tsx
+++ frontend/src/components/properties/PropertyCard.tsx
@@ -1,12 +1,100 @@
 import type { TProperty } from "@/properties";
-import { Card, CardHeader } from "../ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import { SqMtIcon } from "../icons/icons";
+import { FormatMoney } from "@/lib/format";
+import { Button } from "../ui/button";
+import { Heart, Image, Map, Share2 } from "lucide-react";
+import { PropertyLocationMap } from "@/maps/component";
+import { useState } from "react";
 
-export function PropertyCard({ property }: { property: TProperty }) {
+export type TPropertyCardProps = {
+    propData: TProperty;
+    withMap?: boolean;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export function PropertyCard({ propData: property, withMap }: TPropertyCardProps) {
+    if (withMap === undefined) withMap = !!property.lat && !!property.lon;
+
     return (
-        <Card className="">
-            <CardHeader className="p-0">
-                <img src={property.mainImg} className="w-full h-full object-cover rounded-t-lg" />
-            </CardHeader>
-        </Card>
+        <Card className="pt-0 gap-4">
+            {
+                withMap ?
+                    <PropertyCardHeaderWithMap property={property} /> :
+                    <PropertyCardHeader property={property} />
+            }
+            <CardContent className="pt-0 px-4 sm:px-6 space-y-1">
+                <div className="flex items-center justify-between gap-3">
+                    <CardTitle className="text-lg sm:text-2xl font-serif">{FormatMoney(property.price)}</CardTitle>
+                    <div className="flex items-center gap-1.5">
+                        <Button variant="secondary" size="icon">
+                            <Share2 />
+                        </Button>
+                        <Button variant="secondary" size="icon">
+                            <Heart />
+                        </Button>
+                    </div>
+                </div>
+                <CardDescription className="space-y-1">
+                    <div className="flex items-center gap-3 text-current/60 font-semibold tracking-wide">
+                        <div className="flex items-center gap-1 text-sm sm:text-base">
+                            <span>{property.propertyType}</span>
+                        </div>
+                        <div className="flex items-end gap-1.5 text-xs sm:text-sm">
+                            <SqMtIcon className="size-4" />
+                            <span className="font-serif">{property.sqMt.toFixed(2)}mt<sup>2</sup></span>
+                        </div>
+                    </div>
+                    <p className="line-clamp-2 font-medium">
+                        {property.address}, {property.nbHood}. C.P. {property.zip}
+                    </p>
+                </CardDescription>
+            </CardContent>
+        </Card >
+    );
+}
+
+export function PropertyCardHeader({ property }: { property: TProperty }) {
+    return (
+        <CardHeader className="p-0">
+            <div className="relative z-0">
+                <div className="relative z-10 w-full aspect-[2/1] bg-gray-200">
+                    <img
+                        src={property.mainImg || undefined}
+                        alt={`Imagen mostrando la propiedad ubicada en ${property.address}, ${property.nbHood}. C.P. ${property.zip}`}
+                        className="w-full h-full object-cover rounded-t-lg"
+                    />
+                </div>
+            </div>
+        </CardHeader>
+    );
+}
+
+export function PropertyCardHeaderWithMap({ property }: { property: TProperty }) {
+    const [activeTab, setActiveTab] = useState<"map" | "img">("img");
+    const toggleActiveTab = () => {
+        setActiveTab((activeTab) => (activeTab === "map" ? "img" : "map"));
+    }
+
+    return (
+        <CardHeader className="p-0">
+            <div className="relative z-0 w-full h-full overflow-hidden">
+                <div className="absolute z-20 bottom-1.5 right-1.5">
+                    <Button className="grid" onClick={toggleActiveTab} size="icon">
+                        <Map className="col-start-1 row-start-1 size-4 opacity-100 transition-[height,_width,_opacity] data-[show=false]:size-0 data-[show=false]:opacity-0" data-show={activeTab === "img"} />
+                        <Image className="col-start-1 row-start-1 size-0 opacity-0 transition-[height,_width,_opacity] data-[show=true]:size-4 data-[show=true]:opacity-100" data-show={activeTab === "map"} />
+                    </Button>
+                </div>
+                <div className="relative z-10 w-full aspect-[2/1] bg-gray-200 transition-(--transition-appear) duration-500 translate-x-0 opacity-100 data-[is-active=false]:-translate-x-12 data-[is-active=false]:opacity-0 data-[is-active=false]:z-0" data-is-active={activeTab === "img"}>
+                    <img
+                        src={property.mainImg || undefined}
+                        alt={`Imagen mostrando la propiedad ubicada en ${property.address}, ${property.nbHood}. C.P. ${property.zip}`}
+                        className="w-full h-full object-cover rounded-t-lg"
+                    />
+                </div>
+                <div className="absolute inset-0 z-0 rounded-t-lg overflow-hidden transition-(--transition-appear) duration-500 translate-x-12 opacity-0 data-[is-active=true]:translate-x-0 data-[is-active=true]:opacity-100 data-[is-active=true]:z-10" data-is-active={activeTab === "map"}>
+                    <PropertyLocationMap property={property} />
+                </div>
+            </div>
+        </CardHeader>
     );
 }
---
+++ frontend/src/lib/format.ts
@@ -0,0 +1,9 @@
+
+export function FormatMoney(amount: number) {
+    return amount.toLocaleString('es-MX', {
+        style: 'currency',
+        currency: 'MXN',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    });
+}
