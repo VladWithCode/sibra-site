bff081fd35d601e55fc6c8774ce32968c84ba578
vladwb <vladwithcode@gmail.com>
2025-09-18 00:53:02 -0600

    fix: Maps APIProvider is not being setup

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -3,12 +3,15 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '@/queries/queryClient'
 import { SidebarProvider } from '@/components/ui/sidebar'
 import type { IRouteContext } from './-routeContext'
+import { MapsAPIProvider } from '@/maps/component'
 
 export const Route = createRootRouteWithContext<IRouteContext>()({
     component: () => (
         <QueryClientProvider client={queryClient}>
             <SidebarProvider>
-                <Outlet />
+                <MapsAPIProvider>
+                    <Outlet />
+                </MapsAPIProvider>
                 {/* <TanstackDevtools */}
                 {/*     config={{ */}
                 {/*         position: 'bottom-left', */}
