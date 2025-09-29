beba252a9af4077d9992056f0c3ee1a3b7548daa
vladwb <vladwithcode@gmail.com>
2025-09-29 14:19:32 -0600

    chore: add PropertyImageCarousel component

---
+++ frontend/src/components/properties/PropertyImageCarousel.tsx
@@ -0,0 +1,29 @@
+import type { TProperty } from "@/queries/type";
+import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
+import type { PropsWithChildren } from "react";
+
+export function PropertyImageCarousel({ property }: { property: TProperty }) {
+    return (
+        <Carousel>
+            <CarouselContent className="aspect-[3/2]">
+                {property.imgs.map(img => (
+                    <PropertyImageCarouselItem>
+                        <img
+                            src={img}
+                            alt={`Foto de la propiedad ${property.address}, ${property.nbHood}. C.P. ${property.zip}. ${property.city}, ${property.state}`}
+                            className="w-full h-full object-cover object-center"
+                        />
+                    </PropertyImageCarouselItem>
+                ))}
+            </CarouselContent>
+        </Carousel>
+    );
+}
+
+export function PropertyImageCarouselItem({ children }: {} & PropsWithChildren) {
+    return (
+        <CarouselItem className="grow shrink-0 basis-full h-full">
+            {children}
+        </CarouselItem>
+    );
+}
