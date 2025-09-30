bfd1694b9e5d6bb75d4d78386957721efe9d64e7
vladwb <vladwithcode@gmail.com>
2025-09-29 21:19:54 -0600

    chore: fucking redirect

--- frontend/src/routes/_public/propiedades/_listing/index.tsx
+++ frontend/src/routes/_public/propiedades/_listing/index.tsx
@@ -1,15 +1,21 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
+import { useEffect } from 'react';
 
 export const Route = createFileRoute('/_public/propiedades/_listing/')({
     component: RouteComponent,
-    beforeLoad: ({ location }) => {
-        console.log(location.pathname)
+    beforeLoad: () => {
         // Redirect if no contract is provided
         throw redirect({ to: '/propiedades/venta' })
     },
 })
 
 function RouteComponent() {
+    const { setHeaderFloating, setHeaderComplement } = useUIStore();
+    useEffect(() => {
+        setHeaderFloating(false);
+        setHeaderComplement('search');
+    }, []);
+
     return <div>
         <h1>Listado de propiedades</h1>
     </div>
