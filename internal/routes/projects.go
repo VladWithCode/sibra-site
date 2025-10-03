504bce32f2f948ec8489ab43a526efaa432f133e
vladwb <vladwithcode@gmail.com>
2025-10-03 06:22:46 -0600

    feat: project media API

--- internal/routes/projects.go
+++ internal/routes/projects.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -26,6 +27,16 @@ func RegisterProjectRoutes(router *customServeMux) {
 	router.HandleFunc("POST /api/proyectos", CreateProject)
 	router.HandleFunc("PUT /api/proyectos/{id}", UpdateProject)
 	router.HandleFunc("DELETE /api/proyectos/{id}", DeleteProject)
+
+	router.HandleFunc("PUT /api/proyectos/{id}/medios/principal", UploadProjectMainImg)
+	router.HandleFunc("DELETE /api/proyectos/{id}/medios/principal", RemoveProjectMainImg)
+	router.HandleFunc("PUT /api/proyectos/{id}/medios/galeria", UploadProjectGallery)
+	router.HandleFunc("DELETE /api/proyectos/{id}/medios/galeria/{imgID}", RemoveFromProjectGallery)
+	router.HandleFunc("PUT /api/proyectos/{id}/medios/amenidades", UploadProjectAmenity)
+	router.HandleFunc("PUT /api/proyectos/{id}/medios/amenidades/{amenityID}", UpdateProjectAmenity)
+	router.HandleFunc("DELETE /api/proyectos/{id}/medios/amenidades/{amenityID}", DeleteProjectAmenity)
+	router.HandleFunc("PUT /api/proyectos/{id}/medios/disponibilidad", UploadProjectAvailability)
+	router.HandleFunc("DELETE /api/proyectos/{id}/medios/disponibilidad", RemoveProjectAvailability)
 
 	router.HandleFunc("POST /api/proyectos/{id}/socios/{associateID}", AddProjectAssociate)
 	router.HandleFunc("PUT /api/proyectos/{id}/socios/{associateID}", UpdateProjectAssociate)
@@ -731,6 +742,630 @@ func RemoveProjectAssociate(w http.ResponseWriter, r *http.Request) {
 			ErrorMessage: "Ocurrió un error al eliminar la relación de proyecto-asociado",
 		})
 		log.Printf("Failed to delete project associate: %v\n", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]any{
+		"success": true,
+	})
+}
+
+func UploadProjectMainImg(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	projectID := r.PathValue("id")
+
+	err := r.ParseMultipartForm(uploads.MaxImageUploadSize)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "Ocurrió un error al procesar la solicitud. Verifica que la información proporcionada sea correcta",
+		})
+		log.Printf("Error parsing multipart form: %v\n", err)
+		return
+	}
+
+	files := r.MultipartForm.File["file"]
+	if len(files) == 0 {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "Debes proporcionar un archivo.",
+		})
+		return
+	} else if len(files) > 1 {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "Solo puede haber una imagen principal.",
+		})
+		return
+	}
+
+	project, err := db.FindProject(ctx, projectID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(w, http.StatusNotFound, ErrorParams{
+				ErrorMessage: "No se encontró el proyecto",
+			})
+			return
+		}
+
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error inesperado. Intenta de nuevo más tarde.",
+		})
+		log.Printf("Error finding project: %v\n", err)
+		return
+	}
+
+	file := files[0]
+	filename, err := uploads.Upload(&uploads.FileData{
+		Filename: fmt.Sprintf("%s-%s", project.Slug, "main-img"),
+		File:     file,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error inesperado. Intenta de nuevo más tarde.",
+		})
+		log.Printf("Error uploading file: %v\n", err)
+		return
+	}
+	project.MainImg = filename
+
+	err = db.UpdateProject(ctx, project)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error al actualizar el proyecto",
+		})
+		log.Printf("Error updating project: %v\n", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]any{
+		"success": true,
+		"project": project,
+	})
+}
+
+func RemoveProjectMainImg(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	projectID := r.PathValue("id")
+	project, err := db.FindProject(ctx, projectID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(w, http.StatusNotFound, ErrorParams{
+				ErrorMessage: "No se encontró el proyecto",
+			})
+			return
+		}
+
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error inesperado. Intenta de nuevo más tarde.",
+		})
+		log.Printf("Error finding project: %v\n", err)
+		return
+	}
+
+	err = uploads.Delete(project.MainImg)
+	if err != nil && !os.IsNotExist(err) {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error al eliminar la imagen principal",
+		})
+		log.Printf("Error deleting main image: %v\n", err)
+		return
+	}
+
+	project.MainImg = ""
+
+	err = db.UpdateProject(ctx, project)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error al actualizar el proyecto",
+		})
+		log.Printf("Error updating project: %v\n", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]any{
+		"success": true,
+	})
+}
+
+func UploadProjectGallery(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	projectID := r.PathValue("id")
+
+	err := r.ParseMultipartForm(uploads.MaxImageUploadSize * uploads.MaxMultiUploadCount)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "Ocurrió un error al procesar la solicitud. Verifica que la información proporcionada sea correcta",
+		})
+		log.Printf("Error parsing multipart form: %v\n", err)
+		return
+	}
+
+	files := r.MultipartForm.File["file"]
+	if len(files) == 0 {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "Debes proporcionar almenos un archivo.",
+		})
+		log.Printf("Error parsing multipart form: %v\n", err)
+		return
+	}
+
+	project, err := db.FindProject(ctx, projectID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(w, http.StatusNotFound, ErrorParams{
+				ErrorMessage: "No se encontró el proyecto",
+			})
+			return
+		}
+
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error inesperado. Intenta de nuevo más tarde.",
+		})
+		log.Printf("Error finding project: %v\n", err)
+		return
+	}
+
+	written, err := uploads.UploadMultiple(fmt.Sprintf("%s-%s", project.Slug, "galeria"), files)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error inesperado. Intenta de nuevo más tarde.",
+		})
+		log.Printf("Error uploading file: %v\n", err)
+		return
+	}
+
+	for _, file := range written {
+		project.Gallery = append(project.Gallery, file.Filename)
+	}
+
+	err = db.UpdateProject(ctx, project)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error al actualizar el proyecto",
+		})
+		log.Printf("Error updating project: %v\n", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]any{
+		"success": true,
+		"project": project,
+	})
+}
+
+func RemoveFromProjectGallery(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	projectID := r.PathValue("id")
+	imgID := r.PathValue("imgID")
+	project, err := db.FindProject(ctx, projectID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(w, http.StatusNotFound, ErrorParams{
+				ErrorMessage: "No se encontró el proyecto",
+			})
+			return
+		}
+
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error inesperado. Intenta de nuevo más tarde.",
+		})
+		log.Printf("Error finding project: %v\n", err)
+		return
+	}
+
+	var foundImg string
+	newImgs := []string{}
+	for _, img := range project.Gallery {
+		if img == imgID {
+			foundImg = img
+			continue
+		}
+
+		newImgs = append(newImgs, img)
+	}
+
+	if foundImg == "" {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "No se encontró la imagen a eliminar",
+		})
+		return
+	}
+
+	project.Gallery = newImgs
+
+	err = db.UpdateProject(ctx, project)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error al actualizar el proyecto",
+		})
+		log.Printf("Error updating project: %v\n", err)
+		return
+	}
+
+	err = uploads.Delete(foundImg)
+	if err != nil && !os.IsNotExist(err) {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error al eliminar la imagen",
+		})
+		log.Printf("Error deleting image: %v\n", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]any{
+		"success": true,
+	})
+}
+
+func UploadProjectAmenity(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	projectID := r.PathValue("id")
+
+	err := r.ParseMultipartForm(uploads.MaxImageUploadSize)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "Ocurrió un error al procesar la solicitud. Verifica que la información proporcionada sea correcta",
+		})
+		log.Printf("Error parsing multipart form: %v\n", err)
+		return
+	}
+
+	files := r.MultipartForm.File["file"]
+	if len(files) > 1 {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "Solo puedes subir un archivo a la vez.",
+		})
+		log.Printf("Error parsing multipart form: %v\n", err)
+		return
+	}
+
+	project, err := db.FindProject(ctx, projectID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(w, http.StatusNotFound, ErrorParams{
+				ErrorMessage: "No se encontró el proyecto",
+			})
+			return
+		}
+
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error inesperado. Intenta de nuevo más tarde.",
+		})
+		log.Printf("Error finding project: %v\n", err)
+		return
+	}
+
+	amenity := db.ProjectAmenity{
+		ID:   uuid.Must(uuid.NewV7()).String(),
+		Name: r.FormValue("name"),
+		Icon: r.FormValue("icon"),
+		Img:  "",
+	}
+
+	if len(files) > 0 {
+		date := time.Now().Format("2006-01-02T15:04:05")
+		fData := uploads.FileData{
+			Filename: fmt.Sprintf("%s-%s-%s", project.Slug, "amenity", date),
+			File:     files[0],
+		}
+		filename, err := uploads.Upload(&fData)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, ErrorParams{
+				ErrorMessage: "Ocurrió un error inesperado. Intenta de nuevo más tarde.",
+			})
+			log.Printf("Error uploading file: %v\n", err)
+			return
+		}
+
+		amenity.Img = filename
+	}
+
+	project.Amenities = append(project.Amenities, amenity)
+
+	err = db.UpdateProject(ctx, project)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error al actualizar el proyecto",
+		})
+		log.Printf("Error updating project: %v\n", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]any{
+		"success": true,
+		"project": project,
+	})
+
+	go func() {
+		uploads.Delete(amenity.Img)
+	}()
+}
+
+func UpdateProjectAmenity(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	projectID := r.PathValue("id")
+	amenityID := r.PathValue("amenityID")
+
+	err := r.ParseMultipartForm(uploads.MaxImageUploadSize)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "Ocurrió un error al procesar la solicitud. Verifica que la información proporcionada sea correcta",
+		})
+		log.Printf("Error parsing multipart form: %v\n", err)
+		return
+	}
+
+	files := r.MultipartForm.File["file"]
+	if len(files) > 1 {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "Solo puedes subir un archivo a la vez.",
+		})
+		log.Printf("Error parsing multipart form: %v\n", err)
+		return
+	}
+
+	project, err := db.FindProject(ctx, projectID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(w, http.StatusNotFound, ErrorParams{
+				ErrorMessage: "No se encontró el proyecto",
+			})
+			return
+		}
+
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error inesperado. Intenta de nuevo más tarde.",
+		})
+		log.Printf("Error finding project: %v\n", err)
+		return
+	}
+
+	var foundAmenity *db.ProjectAmenity
+	for _, amenity := range project.Amenities {
+		if amenity.ID == amenityID {
+			foundAmenity = &amenity
+			break
+		}
+	}
+
+	if foundAmenity == nil {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "No se encontró la amenidad a actualizar",
+		})
+		return
+	}
+
+	if len(files) > 0 {
+		if foundAmenity.Img != "" {
+			err = uploads.Delete(foundAmenity.Img)
+			if err != nil && !os.IsNotExist(err) {
+				respondWithError(w, http.StatusInternalServerError, ErrorParams{
+					ErrorMessage: "Ocurrió un error al eliminar la imagen actual",
+				})
+				log.Printf("Error deleting image: %v\n", err)
+				return
+			}
+		}
+
+		date := time.Now().Format("2006-01-02T15:04:05")
+		fData := uploads.FileData{
+			Filename: fmt.Sprintf("%s-%s-%s", project.Slug, "amenity", date),
+			File:     files[0],
+		}
+		filename, err := uploads.Upload(&fData)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, ErrorParams{
+				ErrorMessage: "Ocurrió un error inesperado. Intenta de nuevo más tarde.",
+			})
+			log.Printf("Error uploading file: %v\n", err)
+			return
+		}
+
+		foundAmenity.Img = filename
+	}
+
+	foundAmenity.Name = r.FormValue("name")
+	foundAmenity.Icon = r.FormValue("icon")
+
+	newAmenities := []db.ProjectAmenity{}
+	for _, amenity := range project.Amenities {
+		if amenity.ID == amenityID {
+			continue
+		}
+
+		newAmenities = append(newAmenities, amenity)
+	}
+	newAmenities = append(newAmenities, *foundAmenity)
+	project.Amenities = newAmenities
+
+	err = db.UpdateProject(ctx, project)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error al actualizar el proyecto",
+		})
+		log.Printf("Error updating project: %v\n", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]any{
+		"success": true,
+		"project": project,
+	})
+}
+
+func DeleteProjectAmenity(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	projectID := r.PathValue("id")
+	amenityID := r.PathValue("amenityID")
+
+	project, err := db.FindProject(ctx, projectID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(w, http.StatusNotFound, ErrorParams{
+				ErrorMessage: "No se encontró el proyecto",
+			})
+			return
+		}
+
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error inesperado. Intenta de nuevo más tarde.",
+		})
+		log.Printf("Error finding project: %v\n", err)
+		return
+	}
+
+	var foundAmenity *db.ProjectAmenity
+	newAmenities := []db.ProjectAmenity{}
+	for _, amenity := range project.Amenities {
+		if amenity.ID == amenityID {
+			foundAmenity = &amenity
+			continue
+		}
+
+		newAmenities = append(newAmenities, amenity)
+	}
+
+	if foundAmenity == nil {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "No se encontró la imagen a eliminar",
+		})
+		return
+	}
+
+	project.Amenities = newAmenities
+
+	err = db.UpdateProject(ctx, project)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error al actualizar el proyecto",
+		})
+		log.Printf("Error updating project: %v\n", err)
+		return
+	}
+
+	err = uploads.Delete(foundAmenity.Img)
+	if err != nil && !os.IsNotExist(err) {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error al eliminar la imagen",
+		})
+		log.Printf("Error deleting image: %v\n", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]any{
+		"success": true,
+	})
+}
+
+func UploadProjectAvailability(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	projectID := r.PathValue("id")
+
+	err := r.ParseMultipartForm(uploads.MaxImageUploadSize)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "Ocurrió un error al procesar la solicitud. Verifica que la información proporcionada sea correcta",
+		})
+		log.Printf("Error parsing multipart form: %v\n", err)
+		return
+	}
+
+	files := r.MultipartForm.File["file"]
+	if len(files) == 0 {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "Debes proporcionar un archivo.",
+		})
+		log.Printf("Error parsing multipart form: %v\n", err)
+		return
+	} else if len(files) > 1 {
+		respondWithError(w, http.StatusBadRequest, ErrorParams{
+			ErrorMessage: "Solo puedes subir un archivo a la vez.",
+		})
+		log.Printf("Error parsing multipart form: %v\n", err)
+		return
+	}
+
+	project, err := db.FindProject(ctx, projectID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(w, http.StatusNotFound, ErrorParams{
+				ErrorMessage: "No se encontró el proyecto",
+			})
+			return
+		}
+
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error inesperado. Intenta de nuevo más tarde.",
+		})
+		log.Printf("Error finding project: %v\n", err)
+		return
+	}
+
+	fData := uploads.FileData{
+		Filename: fmt.Sprintf("%s-%s", project.Slug, "availability"),
+		File:     files[0],
+	}
+	filename, err := uploads.Upload(&fData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error inesperado. Intenta de nuevo más tarde.",
+		})
+		log.Printf("Error uploading file: %v\n", err)
+		return
+	}
+
+	project.AvailabilityImg = filename
+
+	err = db.UpdateProject(ctx, project)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error al actualizar el proyecto",
+		})
+		log.Printf("Error updating project: %v\n", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, map[string]any{
+		"success": true,
+		"project": project,
+	})
+}
+
+func RemoveProjectAvailability(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	projectID := r.PathValue("id")
+
+	project, err := db.FindProject(ctx, projectID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(w, http.StatusNotFound, ErrorParams{
+				ErrorMessage: "No se encontró el proyecto",
+			})
+			return
+		}
+
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error inesperado. Intenta de nuevo más tarde.",
+		})
+		log.Printf("Error finding project: %v\n", err)
+		return
+	}
+
+	availabilityImg := project.AvailabilityImg
+	project.AvailabilityImg = ""
+
+	err = db.UpdateProject(ctx, project)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error al actualizar el proyecto",
+		})
+		log.Printf("Error updating project: %v\n", err)
+		return
+	}
+
+	err = uploads.Delete(availabilityImg)
+	if err != nil && !os.IsNotExist(err) {
+		respondWithError(w, http.StatusInternalServerError, ErrorParams{
+			ErrorMessage: "Ocurrió un error al eliminar la imagen",
+		})
+		log.Printf("Error deleting image: %v\n", err)
 		return
 	}
 
