168432256251efc8d0e814bc33f25a4dd617a957
vladwb <vladwithcode@gmail.com>
2025-10-03 04:45:17 -0600

    chore: configure vite proxy to serve static files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": "http://localhost:8080",
-            "/static/properties": "http://localhost:8080",
+            "/static": "http://localhost:8080",
         },
     },
 });
