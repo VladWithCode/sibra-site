7086729eadf031bd5a4746268e89e9122260eb76
vladwb <vladwithcode@gmail.com>
2025-10-03 18:30:24 -0600

    fix: incorrect return type for getProjects

--- frontend/src/queries/projects.ts
+++ frontend/src/queries/projects.ts
@@ -1,5 +1,5 @@
 import { queryOptions, type QueryFunctionContext } from "@tanstack/react-query";
-import type { TProject, TProjectAssociate, TProjectAssociateDetailResult, TProjectCheckAccessData, TProjectCheckAccessResult, TProjectDetailResult, TProjectDocsResult } from "./type";
+import type { TProject, TProjectAssociate, TProjectAssociateDetailResult, TProjectCheckAccessData, TProjectCheckAccessResult, TProjectDetailResult, TProjectDocsResult, TProjectListingResult } from "./type";
 
 export const ProjectQueryKeys = {
     all: () => ["projects"] as const,
@@ -70,7 +70,7 @@ export const getProjectAssociateByIdOpts = (id: string) => queryOptions({
     queryFn: getProjectAssociateById,
 });
 
-export async function getProjects(): Promise<TProject[]> {
+export async function getProjects(): Promise<TProjectListingResult> {
     const response = await fetch("/api/proyectos");
     const data = await response.json();
 
