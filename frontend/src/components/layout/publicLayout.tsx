454e44fb2631d7d42b2e513efdac2572258ade5d
vladwb <vladwithcode@gmail.com>
2025-10-04 16:17:13 -0600

    chore: add global not found and error pages
    also add a public layout component for consistency in public pages

---
+++ frontend/src/components/layout/publicLayout.tsx
@@ -0,0 +1,27 @@
+import { useUIStore } from "@/stores/uiStore";
+import { Footer } from "../footer";
+import { Header, HeaderSidebar } from "../header";
+import { cn } from "@/lib/utils";
+
+export function PublicLayout({ children }: {} & React.PropsWithChildren) {
+    const { headerFloating } = useUIStore();
+
+    return (
+        <>
+            <div className="xl:hidden">
+                <HeaderSidebar />
+            </div>
+            <div
+                id="main-content"
+                className={cn(
+                    "relative h-screen w-screen grid grid-cols-1 z-0 overflow-x-hidden overflow-y-auto",
+                    headerFloating ? "grid-rows-[1fr_auto]" : "grid-rows-[1fr_auto_1fr]",
+                )}
+            >
+                <Header />
+                {children}
+                <Footer />
+            </div>
+        </>
+    );
+}
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,10 +1,14 @@
-import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import { Link, Outlet, createRootRouteWithContext, type ErrorComponentProps } from "@tanstack/react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/queries/queryClient";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import type { IRouteContext } from "./-routeContext";
 import { MapsAPIProvider } from "@/maps/component";
 import { Toaster } from "@/components/ui/sonner";
+import { useUIStore } from "@/stores/uiStore";
+import { useEffect } from "react";
+import { PublicLayout } from "@/components/layout/publicLayout";
+import { Button } from "@/components/ui/button";
 
 export const Route = createRootRouteWithContext<IRouteContext>()({
     component: () => (
@@ -28,4 +32,65 @@ export const Route = createRootRouteWithContext<IRouteContext>()({
             </SidebarProvider>
         </QueryClientProvider>
     ),
+    errorComponent: GlobalErrorComponent,
+    notFoundComponent: GlobalNotFound,
 });
+
+function GlobalNotFound() {
+    const { setHeaderFloating, setHeaderComplementProps } = useUIStore();
+
+    useEffect(() => {
+        setHeaderFloating(false);
+        setHeaderComplementProps({ complementType: "none" });
+    }, []);
+
+    return (
+        <PublicLayout>
+            <main className="flex flex-col items-center justify-center gap-6 xl:gap-12 px-6 py-16 xl:py-36">
+                <div className="space-y-6">
+                    <p className="text-9xl xl:text-[10rem] font-bold text-muted-foreground text-center">
+                        404
+                    </p>
+                    <h1 className="text-2xl xl:text-3xl text-muted-foreground">La página que buscas no existe</h1>
+                </div>
+                <p className="xl:text-lg font-medium text-current/80">
+                    Puedes volver al inicio dando click an botón de abajo.
+                </p>
+                <Button className="text-xl text-gray-50 font-semibold bg-sbr-blue" variant="link" asChild size="lg">
+                    <Link to="/" className="py-6 px-12">Volver al inicio</Link>
+                </Button>
+            </main>
+        </PublicLayout>
+    );
+}
+
+function GlobalErrorComponent({ error }: ErrorComponentProps) {
+    const { setHeaderFloating, setHeaderComplementProps } = useUIStore();
+
+    useEffect(() => {
+        setHeaderFloating(false);
+        setHeaderComplementProps({ complementType: "none" });
+
+        console.error("unhandled error: ", error);
+    }, []);
+
+    return (
+        <PublicLayout>
+            <main className="flex flex-col items-center justify-center gap-6 xl:gap-12 px-6 py-16 xl:py-36">
+                <div className="space-y-6">
+                    <p className="text-9xl xl:text-[10rem] font-bold text-muted-foreground text-center">
+                        500
+                    </p>
+                    <h1 className="text-2xl xl:text-3xl text-muted-foreground">Ocurrió un error inesperado</h1>
+                </div>
+                <p className="xl:text-lg font-medium text-current/80">
+                    Parece que algo salió mal. Puedes volver al inicio o contactar a soporte si el problema
+                    persiste.
+                </p>
+                <Button className="text-xl text-gray-50 font-semibold bg-sbr-blue" variant="link" asChild size="lg">
+                    <Link to="/" className="py-6 px-12">Volver al inicio</Link>
+                </Button>
+            </main>
+        </PublicLayout>
+    );
+}
