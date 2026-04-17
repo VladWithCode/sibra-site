755c8d8199ac22498ed873c263318e2f9bd2af1f
vladwb <vladwithcode@gmail.com>
2026-04-16 19:38:52 -0600

    feat: Projects listing page redesign

--- frontend/src/components/projects/ProjectCard.tsx
+++ frontend/src/components/projects/ProjectCard.tsx
@@ -1,42 +1,50 @@
 import { Button } from "../ui/button";
-import { Card, CardContent, CardHeader } from "../ui/card";
-import { LikeProjectButton } from "../properties/likeButton";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import type { TProject } from "@/queries/type";
-import { ShareIcon } from "../icons/icons";
 import { Link } from "@tanstack/react-router";
+import { ArrowRight } from "lucide-react";
+import { cn } from "@/lib/utils";
 
-export function ProjectCard({ project }: { project: TProject, withMap?: boolean }) {
+export function ProjectCard({ project, className }: { project: TProject } & React.ComponentProps<"div">) {
     return (
-        <Card className="py-0 gap-3">
-            <CardHeader className="p-0 bg-gray-100 gap-0">
-                <Link to={`/proyectos/${project.slug}`}>
-                    <img src="/hero.png" alt="" className="w-full aspect-[5/2] object-cover object-center brightness-80 rounded-t-lg" />
-                </Link>
+        <Card className={cn(
+            "group relative overflow-hidden rounded-xl bg-surface-container-lowest shadow-md pt-0",
+            className,
+        )}>
+            <CardHeader className="p-0">
+                <img className="aspect-video w-full object-cover transition-transform duration-700 group-hover:scale-105" data-alt="luxury modern villa architectural rendering with minimalist concrete structure and infinity pool overlooking a valley" src="https://lh3.googleusercontent.com/aida-public/AB6AXuDUBxRvbj8T5r_T0I7b8A6zKkSgkijLeHUkXAZS_HzXNiNdHe3SVfZ5-9P0Gkxs0v38p2CJtaW7_Sq3J3Y5ypUmRagjzqWiPptLd9rAKojFVHLUhTYIWA9cjIONxuHFRahiI4KQUyrmca9FcQbov63UI9Az32xgfzZtG9StvLw9p92JrMbCK1_dY_H8crWLJRxzUWNMKwoOQUIqnb4cVWnHjATqIpGXOsMIG0xaJXuEslsPPQXgiZNPjaTJ-8a4uccvig_zMlBRKZU" />
             </CardHeader>
-            <CardContent className="pt-0 pb-3 px-3 sm:px-3.5 space-y-3">
-                <div className="grid grid-cols-[1fr_auto] auto-rows-min items-start gap-3">
-                    <Link to={`/proyectos/${project.slug}`} className="mt-auto">
-                        <p className="text-sm text-muted-foreground">
-                            Desarrollo SIBRA
-                        </p>
-                        <h3 className="text-lg xs:text-2xl font-bold line-clamp-1">
-                            {project.name}
-                        </h3>
-                    </Link>
-                    <div className="space-y-1">
-                        <div className="flex gap-2 items-center justify-between">
-                            <Button className="shadow" variant="secondary" size="icon">
-                                <ShareIcon className="size-5" />
-                            </Button>
-                            <LikeProjectButton className="shadow" project={project} />
-                        </div>
-                        <Button asChild variant="default" size="sm">
-                            <Link to={`/proyectos/${project.slug}`} className="w-full bg-sbr-blue text-gray-50 font-semibold">
-                                Conocer
-                            </Link>
-                        </Button>
+            <CardContent className="space-y-6">
+                <CardTitle className="space-y-1.5">
+                    {/* <span className="text-sbr-blue font-bold text-xs uppercase tracking-widest block mb-2">Proyecto Insignia</span> */}
+                    <h3 className="font-headline text-2xl font-bold text-on-surface tracking-tight">{project.name}</h3>
+                    <div className="text-on-surface-variant/80 font-light mt-1 line-clamp-2">
+                        {project.description}
                     </div>
+                </CardTitle>
+                <div className="grid grid-cols-2 gap-6 min-h-20 border-y border-outline-variant/30">
+                    <div className="flex flex-col justify-around bg-surface-container p-2 rounded-md">
+                        <p className="text-on-surface-variant text-xs uppercase tracking-tighter">Área Total</p>
+                        <p className="font-bold md:text-lg">12,450 m²</p>
+                    </div>
+                    <div className="flex flex-col justify-around bg-surface-container p-2 rounded-md">
+                        <p className="text-on-surface-variant text-xs uppercase tracking-tighter">Disponibilidad</p>
+                        <p className="font-headline font-bold md:text-lg">14/42 Lotes</p>
+                    </div>
+                    {/* <div> */}
+                    {/*     <p className="text-on-surface-variant text-[10px] uppercase tracking-tighter">Rendimiento Meta</p> */}
+                    {/*     <p className="font-headline font-bold md:text-lg text-primary-container">18.4% APY</p> */}
+                    {/* </div> */}
                 </div>
+                <Button
+                    className="flex justify-between items-center gap-2 text-sbr-blue font-bold underline underline-offset-2 hover:scale-103 active:scale-98"
+                    variant="link"
+                    asChild
+                >
+                    <Link to={"/proyectos/" + project.slug}>
+                        <span>Ver Plan Maestro</span><ArrowRight className="size-4" />
+                    </Link>
+                </Button>
             </CardContent>
         </Card>
     );
--- frontend/src/routes/_public/proyectos/index.tsx
+++ frontend/src/routes/_public/proyectos/index.tsx
@@ -1,9 +1,12 @@
 import { ProjectCard } from '@/components/projects/ProjectCard';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { getProjectsOpts } from '@/queries/projects';
 import { queryClient } from '@/queries/queryClient';
 import { useUIStore } from '@/stores/uiStore';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router'
+import { Compass } from 'lucide-react';
 import { useEffect } from 'react';
 
 export const Route = createFileRoute('/_public/proyectos/')({
@@ -24,28 +27,81 @@ function RouteComponent() {
     }, []);
 
     return (
-        <main className="bg-gray-200">
-            <section className="relative z-0 overflow-hidden">
+        <main>
+            <section className="relative flex items-end overflow-hidden">
                 <div className="absolute inset-0 z-0">
-                    <img src="/sample.webp" alt="" className="w-full h-full object-cover object-center brightness-80" />
+                    <img
+                        alt="Architectural perspective of modern development"
+                        className="w-full h-full object-cover object-center brightness-75"
+                        data-alt="wide angle shot of modern glass skyscrapers reflecting a clear blue sky with architectural symmetry and clean lines"
+                        src="https://lh3.googleusercontent.com/aida-public/AB6AXuBNDdf3krlI0L-hgyR_7Mwc69FIb9E1nmXqLpTqA3Ke5ftUXjDiPwm4Y5jrgLcf1APU1ktJTWtaCGNguBuIibljk-5jO9RM9xDePwrhiVzwN706LZ65I-RzDtQu7BtKTZS-7vqyR24tdVDmEJm71nUehrHKlzZy0KxqHSOa7oTNz3AsQFIuxmbTgaOFrl9esetGp-ciMF0MssBpJoMggWxtj9VZxQR9G9bAju1E8keBpBguXnlE1t15Y2w3Wm4z3ye05IraUDpVjGk"
+                    />
+                    <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/20 to-transparent"></div>
                 </div>
-                <div className="relative z-10 text-gray-50 px-6 sm:px-12 pt-20 sm:pt-24 lg:pt-32 xl:pt-36 2xl:pt-44 pb-12 lg:pb-16 xl:pb-20 2xl:pb-28 bg-linear-to-b from-sbr-blue-dark/60 to-sbr-blue-light/60">
-                    <div className="max-w-6xl mx-auto space-y-6 sm:space-y-8 2xl:space-y-10">
-                        <h1 className="text-3xl sm:text-5xl xl:text-6xl 2xl:text-7xl font-semibold">Proyectos Sibra</h1>
-                        <p className="text-muted font-medium">
-                            En Sibra nos interesa brindarle a nuestros clientes las mejores opciones para
-                            comenzar y expandir su patrimonio. Por eso, nos dedicamos a crear y desarrollar
-                            proyectos inmobiliarios que ofrezcan grandes oportunidades para ellos.
+                <div className="relative z-10 w-full p-6 pt-52 max-w-7xl mx-auto backdrop-blur-xs">
+                    <div className="max-w-2xl text-on-primary">
+                        {/* <span className="inline-block bg-primary-container/60 text-on-primary px-3 py-1 rounded text-xs uppercase tracking-widest mb-6 backdrop-blur-md border border-primary-container"> */}
+                        {/*     Nivel Institucional */}
+                        {/* </span> */}
+                        <h1 className="text-5xl leading-snug mb-3 tracking-tighter">
+                            Construyendo el <br />Futuro del <span className="text-primary-fixed">Suelo Soberano</span>
+                        </h1>
+                        <p className="text-current/80 leading-relaxed max-w-xl">
+                            Portafolios de precisión para el inversor institucional moderno.
                         </p>
                     </div>
                 </div>
             </section>
-            <section className="relative z-0 max-w-7xl grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-6 px-4 py-12 mx-auto">
-                {projects.map((project) => (
-                    <ProjectCard key={project.id} project={project} />
-                ))}
+            <section className="bg-surface p-6 py-12">
+                <div className="max-w-7xl mx-auto space-y-12">
+                    <div className="text-on-surface md:border-l-4 md:border-sbr-blue-light md:pl-6">
+                        <h2 className="flex flex-col gap-3 font-semibold text-4xl tracking-tight leading-none mb-4">
+                            <span className="inline-block">Desarrollos Destacados</span>
+                            <span className="w-1/6 h-1 bg-sbr-green-light inline-block text-[0px]">decoration</span>
+                        </h2>
+                        <p className="text-current/60">Adquisiciones estratégicas de terrenos y comunidades planificadas en curso.</p>
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-12 gap-8">
+                        {
+                            projects.map((project) => (
+                                <ProjectCard project={project} key={project.id} />
+                            ))
+                        }
+                        <div className="md:col-span-4 group bg-surface-container-lowest rounded-xl overflow-hidden border-2 border-primary/5">
+                            <div className="h-full flex flex-col items-center justify-center p-8 text-center bg-surface-container-low space-y-3">
+                                <Compass className="size-8 text-sbr-blue" />
+                                <h3 className="font-headline text-xl font-extrabold text-on-surface">Próximos Portafolios</h3>
+                                <p className="text-outline text-sm leading-relaxed">Accede primero a conversiones de terreno agrícola a residencial fuera del mercado.</p>
+                                <Button
+                                    className="bg-sbr-green text-surface px-8 rounded-md font-bold text-sm hover:bg-primary transition-colors"
+                                >
+                                    Suscribirse a Alertas
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
-
-        </main>
+            <section className="p-6 py-12 bg-primary-container/5">
+                <div className="max-w-5xl mx-auto glass p-6 py-8 rounded-2xl flex flex-col md:flex-row items-center gap-6 border border-white/40 shadow-2xl shadow-primary/5">
+                    <div className="flex-1">
+                        <h2 className="font-headline text-3xl font-extrabold text-on-surface mb-4 tracking-tight">Mantente a la vanguardia del desarrollo.</h2>
+                        <p className="text-on-surface-variant/60">Recibe reportes trimestrales sobre valuación de terrenos y tendencias arquitectónicas emergentes.</p>
+                    </div>
+                    <div className="flex flex-col md:flex-row gap-4 w-full md:w-auto">
+                        <Input
+                            className="md:flex-1 md:w-64"
+                            placeholder="Correo Institucional"
+                            type="email"
+                        />
+                        <Button
+                            className="bg-sbr-green px-8 hover:shadow-lg active:shadow-xs hover:scale-103 active:scale-98 hover:bg-sbr-green-light active:bg-sbr-green"
+                        >
+                            Enviar
+                        </Button>
+                    </div>
+                </div>
+            </section>
+        </main >
     );
 }
