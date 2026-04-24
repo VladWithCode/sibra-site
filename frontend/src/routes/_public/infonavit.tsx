37bc77e019443ddf707444ec2d021a52b010fe61
vladwb <vladwithcode@gmail.com>
2026-04-24 13:49:01 -0600

    chore: remove unused pages

--- frontend/src/routes/_public/infonavit.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_public/infonavit")({
-    component: RouteComponent,
-});
-
-function RouteComponent() {
-    return <div>Hello "/_public/infonavit"!</div>;
-}
--- frontend/src/routes/_public/terrenos.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_public/terrenos")({
-    component: RouteComponent,
-});
-
-function RouteComponent() {
-    return <div>Hello "/_public/terrenos"!</div>;
-}
